9f5b4a0b08aebd3210164a7e8f61f52eb3096893
NaniNNNNani <Trait.code@gmail.com>
2025-11-26 00:30:46 +0000

    1.4

---
+++ Day_03/remove_int.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int remove_int(int arr[], int size, int target)
+{
+    int i, j = 0;
+
+    for (i = 0; i < size; i++)
+    {
+        if (arr[i] != target)
+        {
+            arr[j] = arr[i];
+            j++;
+        }
+    }
+
+    return j;   // new size
+}
